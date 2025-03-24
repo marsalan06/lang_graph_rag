c6702c74e3f0c2a0151a2d22f4afc8112632bf32
marsalan06 <arsalan.9798@gmail.com>
2025-03-24 16:07:02 +0500

    updated config

--- config.py
+++ config.py
@@ -9,6 +9,13 @@ class Config:
     PINECONE_INDEX = os.getenv("PINECONE_INDEX")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     PINECONE_INDEX_HOST = os.getenv("PINECONE_INDEX_HOST")
+    FIREBASE_CREDENTIALS_PATH = os.getenv("FIREBASE_CREDENTIALS_PATH")
+    LMS_AUTH_URL = os.getenv("LMS_AUTH_URL")
+    FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
+    FIREBASE_AUTH_DOMAIN = os.getenv("FIREBASE_AUTH_DOMAIN")
+    FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
+
+
 
     @staticmethod
     def validate():
